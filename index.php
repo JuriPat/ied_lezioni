70ca0386b81e4d3c3a307c748e65868c13f35661
JuriPat <juri.patacchiola@ied.edu>
2023-03-14 14:55:34 +0100

    modifiche quinta lezione

--- index.php
+++ index.php
@@ -12,14 +12,35 @@
 
     <div class="section-inner">
 
+        <?php 
+        $args = array(
+            'posts_per_page' => 1,
+            );
+            
+            $featured_post = new WP_Query( $args );
+
+
+            if($featured_post->have_posts()) {
+                while( $featured_post->have_posts() ) {
+                    $featured_post->the_post(); 
+                    $featured_id = $post->ID; 
+         
+        ?>
+
         <article class="blog-entry content-block">
             <header class="blog-entry__header">
                 <div class="blog-entry__header__category">
-                    <a href="">Featured</a>
+                    <?php the_category(); ?>
                 </div>
-                <h1 class="blog-entry__header__title">Worth A Thousand Words</h1>
-                <time datetime="">April 9, 2020</time>
+                <h1 class="blog-entry__header__title"><?php the_title(); ?></h1> 
+                <time datetime=""><?php the_time( 'f,j,Y' ) ?></time>
             </header>
+        </article>
+
+        <?php }
+               
+        }
+        ?>
 
 
         <ul class="post-list">
---
+++ template-parts/nav-menu.php
@@ -0,0 +1,16 @@
+
+
+
+
+
+<?php
+        if (function_exists('wp_nav_menu') && has_nav_menu( 'primary-navigation' )){
+           
+           wp_nav_menu(array(
+            'container'       => 'nav',
+            'container_class' => 'site-header__nav',
+            'theme_local'     => 'primary-navigation',
+        ) );
+    
+        }
+       ?>
